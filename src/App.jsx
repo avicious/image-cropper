74441ae180f8eb5d05cab73c59f968d04a193fbf
Shubham Kumar <shubham.kr2951@gmail.com>
2026-02-14 19:57:03 +0530

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import FileInput from "./components/FileInput";
+
 const App = () => {
+  const [image, setImage] = useState("");
+
+  const onImageSelected = (selectedImg) => {
+    setImage(selectedImg);
+  };
+
   return (
-    <div>App</div>
+    <div className="container">
+      <FileInput onImageSelected={onImageSelected} />
+      <div>{image}</div>
+    </div>
   );
 };
 
